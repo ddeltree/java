3cc2249b633f536876521e632724cf5328ae08af
Davi Alexandre <deltreertl@gmail.com>
2024-08-29 16:46:42 -0300

    init

---
+++ empresa/src/Colaborador.java
@@ -0,0 +1,18 @@
+public final abstract class Colaborador {
+    String nome;
+    String cpf;
+    double salario;
+    String setor;
+
+    public double getSalario() {
+
+    }
+
+    public void setSalario(double sal) {
+
+    }
+
+    public double calculaSalario() {
+
+    }
+}
---
+++ empresa/src/Principal.java
@@ -0,0 +1,6 @@
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        Colaborador tecnico = new Tecnico();
+        Colaborador vendedor = new Vendedor();
+    }
+}
